78d20a84b58cc030312309b9d3d723142e133d21
arlegs <86801815+arlegs@users.noreply.github.com>
2022-10-26 00:17:22 -0300

    Criação da empty state, tratamento de erro em heuristica sem itens

--- src/pages/Heuristic/index.jsx
+++ src/pages/Heuristic/index.jsx
@@ -12,6 +12,7 @@ import { ItemVerCard } from "../../components/ItemVerCard/ItemVerCard";
 import { useIntersection } from "../../hooks/useIntersection";
 import { fetcher } from "../../services/fetcher";
 import "./styles.scss";
+import emptyState from "@/assets/empty.png";
 
 import { WithScrollReveal } from "@/components/WithScrollReveal";
 
@@ -31,6 +32,8 @@ export function Heuristic() {
   const itens = data?.itens;
 
   const heuristica = itens?.[0]?.heuristica;
+
+  console.log(heuristica);
 
   const { data: dates } = useSWR(
     `/itensverificacao/${heuristicId}?pagina=${page}&tam_pagina=15`,
@@ -63,68 +66,141 @@ export function Heuristic() {
   const checklistId = heuristica?.checklist;
   const categoryId = heuristica?.categoria;
 
-  return (
-    <div className="Heuristic">
-      <div className="header-heuristic">
-        <div className="content-wrapper-heuristic">
-          <div className="content-heuristic">
-            <div className="back">
-              <Link to={`/checklist/categoria/${heuristica?.categoria}`}>
-                <ArrowLeft size={24} /> Heurística
-              </Link>
-            </div>
-            <div>
-              <h2 className="title-heuristic">{heuristica?.nome}</h2>
-              <p className="description-heuristic">{heuristica?.descricao}</p>
-              <div className="datas">
-                <p> {count_all} Itens de verificação</p>
-                <p>
-                  Fonte utilizada:{" "}
-                  <a target="_blank" href={heuristica?.fonte}>
-                    {heuristica?.tipo_fonte}
-                  </a>
-                </p>
+  const { data: heuristica_data } = useSWR(
+    `/heuristica/${heuristicId}`,
+    fetcher
+  );
+
+  if (itens) {
+    return (
+      <div className="Heuristic">
+        <div className="header-heuristic">
+          <div className="content-wrapper-heuristic">
+            <div className="content-heuristic">
+              <div className="back">
+                <Link to={`/checklist/categoria/${heuristica?.categoria}`}>
+                  <ArrowLeft size={24} /> Heurística
+                </Link>
+              </div>
+              <div>
+                <h2 className="title-heuristic">{heuristica?.nome}</h2>
+                <p className="description-heuristic">{heuristica?.descricao}</p>
+                <div className="datas">
+                  <p> {count_all} Itens de verificação</p>
+                  <p>
+                    Fonte utilizada:{" "}
+                    <a target="_blank" href={heuristica?.fonte}>
+                      {heuristica?.tipo_fonte}
+                    </a>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="breadcrumb">
-        <Link to={`/checklist/${checklistId}`}>
+        <div className="breadcrumb">
+          <Link to={`/checklist/${checklistId}`}>
+            <div className="item">
+              <Folders size={24} /> <p>Lista de categorias</p>
+            </div>
+          </Link>
+          <CaretRight weight="bold" />
+          <Link to={`/checklist/categoria/${categoryId}`} key={categoryId}>
+            <div className="item">
+              <FileText size={24} /> <p>Lista de heurísticas</p>
+            </div>
+          </Link>
+          <CaretRight weight="bold" />
           <div className="item">
-            <Folders size={24} /> <p>Lista de categorias</p>
+            <ListChecks size={24} /> <p>Lista de itens de verificação</p>
           </div>
-        </Link>
-        <CaretRight weight="bold" />
-        <Link to={`/checklist/categoria/${categoryId}`} key={categoryId}>
-          <div className="item">
-            <FileText size={24} /> <p>Lista de heurísticas</p>
-          </div>
-        </Link>
-        <CaretRight weight="bold" />
-        <div className="item">
-          <ListChecks size={24} /> <p>Lista de itens de verificação</p>
+        </div>
+
+        <div className="itenslist">
+          {!!itens &&
+            itens?.slice(0, count_all)?.map((item, i) => (
+              <WithScrollReveal key={item._id} delay={100}>
+                <ItemVerCard
+                  ask={item.pergunta}
+                  id={`checkbox-${item._id}`}
+                  key={item.id}
+                />
+              </WithScrollReveal>
+            ))}
         </div>
       </div>
+    );
+  } else {
+    const categoria = heuristica_data?.categoria?._id;
 
-      <div className="itenslist">
-        {!!itens &&
-          itens?.slice(0, count_all)?.map((item, i) => (
-            <WithScrollReveal key={item._id} delay={100}>
-              <ItemVerCard
-                ask={item.pergunta}
-                id={`checkbox-${item._id}`}
-                key={item.id}
-              />
-            </WithScrollReveal>
-          ))}
-        {hasMore && (
-          <p ref={instanceRef} className="loading">
-            Carregando itens de verificação
-          </p>
-        )}
+    console.log("categoria", categoria);
+
+    const checklist = heuristica_data?.checklist?._id;
+
+    return (
+      <div className="Heuristic">
+        <div className="header-heuristic">
+          <div className="content-wrapper-heuristic">
+            <div className="content-heuristic">
+              <div className="back">
+                <Link to={`/checklist/categoria/${categoria}`}>
+                  <ArrowLeft size={24} /> Heurística
+                </Link>
+              </div>
+              <div>
+                <h2 className="title-heuristic">{heuristica_data?.nome}</h2>
+                <p className="description-heuristic">
+                  {heuristica_data?.descricao}
+                </p>
+                <div className="datas">
+                  <p> {count_all} Itens de verificação</p>
+                  <p>
+                    Fonte utilizada:{" "}
+                    <a target="_blank" href={heuristica_data?.fonte}>
+                      {heuristica_data?.tipo_fonte}
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="breadcrumb">
+          <Link to={`/checklist/${checklist}`}>
+            <div className="item">
+              <Folders size={24} /> <p>Lista de categorias</p>
+            </div>
+          </Link>
+          <CaretRight weight="bold" />
+          <Link to={`/checklist/categoria/${categoria}`}>
+            <div className="item">
+              <FileText size={24} /> <p>Lista de heurísticas</p>
+            </div>
+          </Link>
+          <CaretRight weight="bold" />
+          <div className="item">
+            <ListChecks size={24} /> <p>Lista de itens de verificação</p>
+          </div>
+        </div>
+
+        <div className="empty-state">
+          <div className="Colaboration">
+            <div className="imagem-empty">
+              <img src={emptyState} />
+            </div>
+            <div className="text-colaborations">
+              <p className="title-c">Ainda não temos nenhum item aqui...</p>
+              <p className="subtitle-c">
+                Você conhece algum conteúdo sobre? Se quiser nos <br /> ajudar,
+                compartilhe cosnoco.
+              </p>
+            </div>
+            <Link to="/contribua">Contribua conosco</Link>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
