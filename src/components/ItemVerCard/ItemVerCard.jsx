9e9624ce566eff705acd42d7f73d775eef501ce1
arlegs <86801815+arlegs@users.noreply.github.com>
2022-11-08 16:41:28 -0300

    Ajuste icone de copiar

--- src/components/ItemVerCard/ItemVerCard.jsx
+++ src/components/ItemVerCard/ItemVerCard.jsx
@@ -1,4 +1,4 @@
-import { Clipboard } from "phosphor-react";
+import { Copy } from "phosphor-react";
 import "./styles.scss";
 import { Checkbox } from "@mui/material";
 import { useState } from "react";
@@ -46,7 +46,7 @@ export function ItemVerCard({ ask, id }) {
       </div>
       <CopyToClipboard text={ask} onCopy={(_, result) => notify(result)}>
         <button>
-          <Clipboard size={24} />
+          <Copy size={24} />
         </button>
       </CopyToClipboard>
     </div>
