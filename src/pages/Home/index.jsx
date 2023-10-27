9b49fb5ed6e875ce396ff65f3d800f9d961fce60
arlegs <86801815+arlegs@users.noreply.github.com>
2023-10-27 16:56:48 -0300

    hotjar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,12 +11,15 @@ import { Colaboration } from "@/components/Colaboration";
 import { MHSection } from "./MHSection";
 import { JMSection } from "./JMSection";
 
-import Hotjar from "@hotjar/browser";
-
-const siteId = 3712959;
+const siteId = 123;
 const hotjarVersion = 6;
 
 Hotjar.init(siteId, hotjarVersion);
+
+// Initializing with `debug` option:
+Hotjar.init(siteId, hotjarVersion, {
+  debug: true,
+});
 
 export function Home() {
   return (
