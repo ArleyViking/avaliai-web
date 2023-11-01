830e3c9b47e2349f2f99e7aeb32c106b2ea8e56c
arlegs <86801815+arlegs@users.noreply.github.com>
2023-11-01 15:57:59 -0300

    Adicionando track no card de recomendação de checklist

--- src/pages/Home/Recommendation/CardChecklist/index.jsx
+++ src/pages/Home/Recommendation/CardChecklist/index.jsx
@@ -1,13 +1,21 @@
+import React from "react";
+import mixpanel from "mixpanel-browser"; // Importe o Mixpanel
+
 import "./styles.scss";
 
-export function CardChecklist({ title, descrition, imagem }) {
+export function CardChecklist({ title, description, image }) {
+  const trackCardClick = () => {
+    // Registre o evento personalizado quando o card for clicado
+    mixpanel.track("Card Clicado", { title, description });
+  };
+
   return (
-    <div className="CardChecklist">
-      <img className="Imagem" src={imagem} />
+    <div className="CardChecklist" onClick={trackCardClick}>
+      <img className="Imagem" src={image} />
 
       <div className="text">
         <p className="title-card">{title}</p>
-        <p className="descrition-card">{descrition}</p>
+        <p className="description-card">{description}</p>
       </div>
     </div>
   );
