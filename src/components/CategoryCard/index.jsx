d914fa29a8069f347527a64de29e8936f4237ea5
arlegs <86801815+arlegs@users.noreply.github.com>
2023-11-03 20:54:40 -0300

    Ajuste 1

--- src/components/CategoryCard/index.jsx
+++ src/components/CategoryCard/index.jsx
@@ -3,13 +3,13 @@ import "./styles.scss";
 import mixpanel from "mixpanel-browser"; // Importe o Mixpanel
 
 export function CategoryCard({ title, num_heu, num_itens }) {
-  const trackCardClick = () => {
+  const trackCardClickCat = () => {
     // Registre o evento personalizado quando o card for clicado
     mixpanel.track({ "Categoria:": title }, { title, num_heu });
   };
 
   return (
-    <div className="CategoryCard" onClick={trackCardClick}>
+    <div className="CategoryCard" onClick={trackCardClickCat}>
       <div className="Content">
         <div className="title">
           <p>Categoria</p>
